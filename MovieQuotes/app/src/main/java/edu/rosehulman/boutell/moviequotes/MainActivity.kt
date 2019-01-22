62a9659cc740f8bb4b83b9dfcc40bf3861b23b3c
Matt Boutell <boutell@gmail.com>
2019-01-21 20:05:28 -0500

    Starting code

---
+++ MovieQuotes/app/src/main/java/edu/rosehulman/boutell/moviequotes/MainActivity.kt
@@ -0,0 +1,138 @@
+package edu.rosehulman.boutell.moviequotes
+
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import android.support.design.widget.FloatingActionButton
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),
+    SplashFragment.OnLoginButtonPressedListener {
+
+    // TODO: Create instance of FirebaseAuth and an AuthStateListener
+
+    // Request code for launching the sign in Intent.
+    private val RC_SIGN_IN = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        initializeListeners()
+    }
+
+    // TODO: add (and remove) an auth state listener upon start (and stop).
+
+    private fun initializeListeners() {
+        // TODO: Create an AuthStateListener that passes the UID
+        // to the MovieQuoteFragment if the user is logged in
+        // and goes back to the Splash fragment otherwise.
+        // See https://firebase.google.com/docs/auth/users#the_user_lifecycle
+
+
+    }
+
+    private fun switchToSplashFragment() {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_container, SplashFragment())
+        ft.commit()
+    }
+
+    private fun switchToMovieQuoteFragment(uid: String) {
+        val ft = supportFragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_container, MovieQuoteFragment.newInstance(uid))
+        ft.commit()
+    }
+
+    override fun onLoginButtonPressed() {
+        launchLoginUI()
+    }
+
+    private fun launchLoginUI() {
+        // TODO: Build a login intent and startActivityForResult(intent, ...)
+        // For details, see https://firebase.google.com/docs/auth/android/firebaseui#sign_in
+
+   }
+
+    fun getFab(): FloatingActionButton {
+        return fab
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                // TODO: Sign out.
+
+                true
+            }
+            R.id.action_increase_font_size -> {
+                changeFontSize(4)
+                true
+            }
+            R.id.action_decrease_font_size -> {
+                changeFontSize(-4)
+                true
+            }
+            R.id.action_settings -> {
+                getWhichSettings()
+                true
+            }
+            R.id.action_clear -> {
+                confirmClear()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    private fun changeFontSize(delta: Int) {
+        // Increase the font size by delta sp
+//        var currentSize = quote_text_view.textSize / resources.displayMetrics.scaledDensity
+//        currentSize += delta
+//        quote_text_view.textSize = currentSize
+//        movie_text_view.textSize = currentSize
+    }
+
+    private fun confirmClear() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.confirm_delete_title))
+        builder.setMessage(getString(R.string.confirm_delete_message))
+        builder.setPositiveButton(android.R.string.ok) { _, _ ->
+            // updateQuote(defaultMovieQuote)
+        }
+        builder.setNegativeButton(android.R.string.cancel, null)
+        builder.create().show()
+    }
+
+    private fun getWhichSettings() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.dialog_which_settings_title))
+        // For others, see https://developer.android.com/reference/android/provider/Settings
+        builder.setItems(R.array.settings_types) { _, index ->
+            var actionConstant = when (index) {
+                0 -> Settings.ACTION_SOUND_SETTINGS
+                1 -> Settings.ACTION_SEARCH_SETTINGS
+                else -> Settings.ACTION_SETTINGS
+            }
+            startActivity(Intent(actionConstant))
+        }
+        builder.create().show()
+    }
+}
